c7d9921420f8bbecc4909df5baa16e2edce29d3e
Nate Traugh <n.traugh@yahoo.com>
2022-10-25 13:23:31 -0400

    react practice with forms, events, and state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(input) {
+      setList([...list, input])
+      setInput("")
+    }
+  }
+
+  const deleteMe = (idx) => {
+
+    setList(list.filter((item) => item !== list[idx]))
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input value={input} placeholder='Search for something' onChange={(e) => setInput(e.target.value)}></input>
+        <button type='submit'>Submit</button>
+      </form>
+      {console.log(list)}
+      {list.map((list, idx) => (
+        <div key={idx}>
+          {list}
+          <button onClick={() => deleteMe(idx)}>
+            Delete Me
+          </button>
+        </div>
+      ))}
+      
     </div>
   );
 }
